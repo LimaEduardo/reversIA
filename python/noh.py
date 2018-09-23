f077b32ec768c910e3721b8a94af80087279e3da
gabriel-gro <gabriel.ribolive@gmail.com>
2018-09-23 10:17:17 -0300

    impressao da arvore nv 0

--- python/noh.py
+++ python/noh.py
@@ -1,13 +1,14 @@
 from inteligence import *
 import copy
 
-class Noh:
-    matrizGame = []
-    dicPossiveisJogadas = {}
-    cor = None
-    nohPai = None
-    nohFilhos = []
+def ehRaiz(noh):
+    if noh.nohPai == None:
+        return False
+    else:
+        return True
 
+class Noh:
+    
     def __init__(self, matrizGame, cor, nohPai = None):
         self.matrizGame = copy.deepcopy(matrizGame)
         self.nohPai = nohPai
@@ -19,24 +20,7 @@ class Noh:
         self.dicPossiveisJogadas = getDicionarioDePossibilidades(copy.deepcopy(self.matrizGame), self.cor)
 
     def __str__(self):
-        saida = "\n " + self.cor + " "
-        for col in range(len(self.matrizGame[0])):
-            saida += " " + str(col) + " "
-        saida += "\n"
-        for indice, linha in enumerate(self.matrizGame):
-            saida += " " +str(indice)+ " "
-            for valor in linha:
-                if valor == '':
-                    saida += " ° "
-                else:
-                    saida += " " + valor + " "
-            saida += "\n"
-        #saida += "\n"
-        for chave in self.dicPossiveisJogadas:
-            saida += " \'"+ chave + "\' "
-        saida += "\n"
-        saida += str(self.nohFilhos)
-        return saida
+        return getPrint()
 
     def getPrint(self):
         saida = "\n   "
@@ -55,6 +39,7 @@ class Noh:
         for chave in self.dicPossiveisJogadas:
             saida += " \'"+ chave + "\' "
         saida += "\n"
-        saida += str(self.nohFilhos)
+        saida += "Filhos: " + str(self.nohFilhos)
+        saida += "\nPai: " + str(ehRaiz(self))
         return saida
     
--- python/tree.py
+++ python/tree.py
@@ -11,26 +11,28 @@ class Tree:
         print("-----------------------------")
         print("Nivel da arvore: ", self.nivel)
         print("-----------------------------")
-        print(self.raiz)
-        self.mountTree()
-        print(self.imprimeArvore(self.raiz))
+        self.mountTree(self.raiz, 0)
+        print(self)
 
-    def mountTree(self, nivel = 0):
-        for chave in (self.raiz.dicPossiveisJogadas):
+    def mountTree(self, noh, nivel = 0):
+        for chave in (noh.dicPossiveisJogadas):
             # pega posicoes de peças inimigas que se transformarao em suas
-            listPecasInimigas = self.raiz.dicPossiveisJogadas[chave]
+            listPecasInimigas = noh.dicPossiveisJogadas[chave]
 
             # pega matriz resultante apos uma jogada
-            matrizResultante = getMatrizJogadaRealizada(copy.deepcopy(self.raiz.matrizGame), listPecasInimigas,self.raiz.cor)
+            matrizResultante = getMatrizJogadaRealizada(copy.deepcopy(noh.matrizGame), listPecasInimigas,noh.cor)
 
             # Cria uma possivel jogada e passa a vez ao adversario (notCor)
             # pegando matriz resultante da sua jogada, cria um noh para jogada seguinte
-            self.raiz.nohFilhos.append(Noh(matrizResultante, notCor(self.raiz.cor), self.raiz))
+            noh.nohFilhos.append(Noh(matrizResultante, notCor(noh.cor), noh))
+            #print(noh.nohFilhos)
 
         self.nivel = nivel
 
     def __str__(self):
-        return self.imprimeArvore(self.raiz)
+        saida = self.imprimeArvore(self.raiz)
+        saida += self.raiz.getPrint()
+        return saida
 
     def imprimeArvore(self, nohAtual):
         saida = ""
