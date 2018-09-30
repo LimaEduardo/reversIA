df63b057c1de2e74d9714301ff93f3fb3a23bb35
limaeduardo <eduardo.fernandodelima@gmail.com>
2018-09-30 20:26:21 -0300

    ia funcionando no front

--- src/pythonAction.js
+++ src/pythonAction.js
@@ -15,7 +15,7 @@ export function getIAPlay(table, color){
     console.log(table,color)
     return new Promise((resolve, reject) => {
         axios.post('http://localhost:5000/get_jogada_ia', {table, color}, { crossdomain: true }).then((response) => {
-            console.log(response.data)
+            resolve(response.data)
         }, (error) => {
             console.log(error)
             reject(error)
