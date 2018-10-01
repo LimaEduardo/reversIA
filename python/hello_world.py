efea2dbdfbc9f5c8d8179bc3d57b27a67c87c468
gabriel-gro <gabriel.ribolive@gmail.com>
2018-09-30 21:15:24 -0300

    firulinha no hello world

--- python/hello_world.py
+++ python/hello_world.py
@@ -3,30 +3,49 @@ from tree import Tree
 from noh import Noh
 
 def main():
-    matTable = [['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', 'B', 'P', '', '', ''], ['', '', '', 'P', 'B', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '']]
-    # matErro = [['', '', '', '', 'B', '', '', ''], ['', '', 'B', 'B', 'B', '', '', ''], ['', '', 'B', 'B', 'P', '', '', ''], ['', '', '', 'P', 'B', 'P', '', ''], ['', '', '', 'P', 'B', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '']]
-    cor = 'P'
-    # print(getDicionarioDePossibilidades(matErro, cor))
+    matTable = []
+    # matTable.append(['', '', '', '', '', '', '', '')
+    # matTable.append(['', '', '', '', '', '', '', '')
+    # matTable.append(['', '', '', '', '', '', '', '')
+    # matTable.append(['', '', '', 'B', 'P', '', '', '')
+    # matTable.append(['', '', '', 'P', 'B', '', '', '')
+    # matTable.append(['', '', '', '', '', '', '', '')
+    # matTable.append(['', '', '', '', '', '', '', '')
+    # matTable.append(['', '', '', '', '', '', '', ''])
 
-    # print(Noh(matErro, cor, 0))
+
+    matTable.append(["", "", "B", "P", "J", "", "", ""])
+    matTable.append(["", "", "B", "P", "", "", "", ""])
+    matTable.append(["", "", "B", "P", "J", "", "", ""])
+    matTable.append(["", "", "", "B", "P", "", "", ""])
+    matTable.append(["", "", "", "P", "B", "", "", ""])
+    matTable.append(["", "", "", "", "", "", "", ""])
+    matTable.append(["", "", "", "", "", "", "", ""])
+    matTable.append(["", "", "", "", "", "", "", ""])
+
     
-    i = 0
-    print(i)    
-    myTree = Tree(matTable, cor, 3)
-    pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
+    cor = 'P'
+    print(getDicionarioDePossibilidades(matTable, cor))
 
-    while i != 60 and len(pecasMelhorJogada) > 0 :      
-        matTable = getMatrizJogadaRealizada(matTable, pecasMelhorJogada, cor)
-        print(Noh(matTable, cor))           
-        cor = notCor(cor)
+    print(Noh(matTable, cor, 0))
+    
+    # i = 0
+    # print(i)    
+    # myTree = Tree(matTable, cor, 3)
+    # pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
 
-        i += 1
-        print(i)    
-        myTree = Tree(matTable, cor, 3)
-        pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
+    # while i != 60 and len(pecasMelhorJogada) > 0 :      
+    #     matTable = getMatrizJogadaRealizada(matTable, pecasMelhorJogada, cor)
+    #     print(Noh(matTable, cor))           
+    #     cor = notCor(cor)
 
-    if len(pecasMelhorJogada) == 0:
-        print("END GAME")
-    else:
-        print("aqui deu caca; lin 31 | Hello world")
+    #     i += 1
+    #     print(i)    
+    #     myTree = Tree(matTable, cor, 3)
+    #     pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
+
+    # if len(pecasMelhorJogada) == 0:
+    #     print("END GAME")
+    # else:
+    #     print("aqui deu caca; lin 31 | Hello world")
 main()
