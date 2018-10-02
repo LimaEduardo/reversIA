f638fc4e9b07db46c997f800329bce530c9fc7de
limaeduardo <eduardo.fernandodelima@gmail.com>
2018-10-02 08:40:19 -0300

    Add readme

--- python/hello_world.py
+++ python/hello_world.py
@@ -4,24 +4,24 @@ from noh import Noh
 
 def main():
     matTable = []
-    # matTable.append(['', '', '', '', '', '', '', '')
-    # matTable.append(['', '', '', '', '', '', '', '')
-    # matTable.append(['', '', '', '', '', '', '', '')
-    # matTable.append(['', '', '', 'B', 'P', '', '', '')
-    # matTable.append(['', '', '', 'P', 'B', '', '', '')
-    # matTable.append(['', '', '', '', '', '', '', '')
-    # matTable.append(['', '', '', '', '', '', '', '')
-    # matTable.append(['', '', '', '', '', '', '', ''])
+    matTable.append(['', '', '', '', '', '', '', ''])
+    matTable.append(['', '', '', '', '', '', '', ''])
+    matTable.append(['', '', '', '', '', '', '', ''])
+    matTable.append(['', '', '', 'B', 'P', '', '', ''])
+    matTable.append(['', '', '', 'P', 'B', '', '', ''])
+    matTable.append(['', '', '', '', '', '', '', ''])
+    matTable.append(['', '', '', '', '', '', '', ''])
+    matTable.append(['', '', '', '', '', '', '', ''])
 
 
-    matTable.append(["", "", "B", "P", "J", "", "", ""])
-    matTable.append(["", "", "B", "P", "", "", "", ""])
-    matTable.append(["", "", "B", "P", "J", "", "", ""])
-    matTable.append(["", "", "", "B", "P", "", "", ""])
-    matTable.append(["", "", "", "P", "B", "", "", ""])
-    matTable.append(["", "", "", "", "", "", "", ""])
-    matTable.append(["", "", "", "", "", "", "", ""])
-    matTable.append(["", "", "", "", "", "", "", ""])
+    # matTable.append(["", "", "B", "P", "J", "", "", ""])
+    # matTable.append(["", "", "B", "P", "", "", "", ""])
+    # matTable.append(["", "", "B", "P", "J", "", "", ""])
+    # matTable.append(["", "", "", "B", "P", "", "", ""])
+    # matTable.append(["", "", "", "P", "B", "", "", ""])
+    # matTable.append(["", "", "", "", "", "", "", ""])
+    # matTable.append(["", "", "", "", "", "", "", ""])
+    # matTable.append(["", "", "", "", "", "", "", ""])
 
     
     cor = 'P'
@@ -29,23 +29,23 @@ def main():
 
     print(Noh(matTable, cor, 0))
     
-    # i = 0
-    # print(i)    
-    # myTree = Tree(matTable, cor, 3)
-    # pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
+    i = 0
+    print(i)    
+    myTree = Tree(matTable, cor, 3)
+    pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
 
-    # while i != 60 and len(pecasMelhorJogada) > 0 :      
-    #     matTable = getMatrizJogadaRealizada(matTable, pecasMelhorJogada, cor)
-    #     print(Noh(matTable, cor))           
-    #     cor = notCor(cor)
+    while i != 60 and len(pecasMelhorJogada) > 0 :      
+        matTable = getMatrizJogadaRealizada(matTable, pecasMelhorJogada, cor)
+        print(Noh(matTable, cor))           
+        cor = notCor(cor)
 
-    #     i += 1
-    #     print(i)    
-    #     myTree = Tree(matTable, cor, 3)
-    #     pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
+        i += 1
+        print(i)    
+        myTree = Tree(matTable, cor, 3)
+        pecasMelhorJogada =  myTree.pecasAVirarJogadaAtual
 
-    # if len(pecasMelhorJogada) == 0:
-    #     print("END GAME")
-    # else:
-    #     print("aqui deu caca; lin 31 | Hello world")
+    if len(pecasMelhorJogada) == 0:
+        print("END GAME")
+    else:
+        print("aqui deu caca; lin 31 | Hello world")
 main()
